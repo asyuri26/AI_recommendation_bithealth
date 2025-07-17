fe751fe39c1f336eed1092f16784214f40b4e523
M Asyarie Fauzie <126375330+asyuri26@users.noreply.github.com>
2025-07-17 15:07:17 +0700

    Update config.py

--- llm_utils/config.py
+++ llm_utils/config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 # Muat isi .env
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
